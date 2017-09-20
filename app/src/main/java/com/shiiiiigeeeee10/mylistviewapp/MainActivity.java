788d4872a26c75014ec873717ca0e97d7aab36e7
shigeru10 <shiiiiigeeeee10@gmail.com>
2017-09-20 15:51:03 +0900

    create UserAdapter

--- app/src/main/java/com/shiiiiigeeeee10/mylistviewapp/MainActivity.java
+++ app/src/main/java/com/shiiiiigeeeee10/mylistviewapp/MainActivity.java
@@ -1,7 +1,12 @@
 package com.shiiiiigeeeee10.mylistviewapp;
 
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.widget.Adapter;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
@@ -16,18 +21,80 @@ public class MainActivity extends AppCompatActivity {
 
         ListView myListView = (ListView) findViewById(R.id.myListView);
 
-        ArrayList<String> items = new ArrayList<>();
-        for (int i = 0; i < 30; i++) {
-//            items.add("item-" + i);
+        ArrayList<User> users = new ArrayList<>();
+
+        int[] icons = {
+                R.mipmap.ic_launcher,
+                R.mipmap.ic_launcher,
+                R.mipmap.ic_launcher
+        };
+
+        String[] names = {
+                "John",
+                "Bob",
+                "Andy"
+        };
+
+        String[] locs = {
+                "USA",
+                "JPN",
+                "AUS"
+        };
+
+        for (int i = 0; i < icons.length; i++) {
+            User user = new User();
+            user.setIcon(BitmapFactory.decodeResource(
+                    getResources(),
+                    icons[i]
+                )
+            );
+
+            user.setName(names[i]);
+            user.setLoc(locs[i]);
         }
 
-        ArrayAdapter<String> adapter = new ArrayAdapter<String>(
-                this,
-                R.layout.list_item,
-                items
-        );
+        UserAdapter adapter = new UserAdapter(this, 0, users);
+    }
 
-        myListView.setEmptyView(findViewById(R.id.emptyView));
-        myListView.setAdapter(adapter);
+    public class UserAdapter extends ArrayAdapter<User> {
+        private LayoutInflater layoutInflater;
+
+        public UserAdapter(Context c, int id, ArrayList<User> users) {
+            super(c, id, users);
+            this.layoutInflater = (LayoutInflater) c.getSystemService(
+                    Context.LAYOUT_INFLATER_SERVICE
+            );
+        }
+
+    }
+
+    public class User {
+        private Bitmap icon;
+        private String name;
+        private String loc;
+
+        public Bitmap getIcon() {
+            return icon;
+        }
+
+        public void setIcon(Bitmap icon) {
+            this.icon = icon;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+
+        public String getLoc() {
+            return loc;
+        }
+
+        public void setLoc(String loc) {
+            this.loc = loc;
+        }
     }
 }
