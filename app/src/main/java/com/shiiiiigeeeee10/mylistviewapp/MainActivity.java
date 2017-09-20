850d8a22d98ce4eb8ab146cc8cdc4319748e58f9
shigeru10 <shiiiiigeeeee10@gmail.com>
2017-09-20 14:55:13 +0900

    display data

--- app/src/main/java/com/shiiiiigeeeee10/mylistviewapp/MainActivity.java
+++ app/src/main/java/com/shiiiiigeeeee10/mylistviewapp/MainActivity.java
@@ -2,6 +2,10 @@ package com.shiiiiigeeeee10.mylistviewapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,20 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ListView myListView = (ListView) findViewById(R.id.myListView);
+
+        ArrayList<String> items = new ArrayList<>();
+        for (int i = 0; i < 30; i++) {
+            items.add("item-" + i);
+        }
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(
+                this,
+                R.layout.list_item,
+                items
+        );
+
+        myListView.setAdapter(adapter);
     }
 }
