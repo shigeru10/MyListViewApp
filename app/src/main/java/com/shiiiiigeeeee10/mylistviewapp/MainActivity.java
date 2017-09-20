ff61ce0cd3977d83b0585cda619cf693e39b0dab
shigeru10 <shiiiiigeeeee10@gmail.com>
2017-09-20 14:10:53 +0900

    first commit

---
+++ app/src/main/java/com/shiiiiigeeeee10/mylistviewapp/MainActivity.java
@@ -0,0 +1,13 @@
+package com.shiiiiigeeeee10.mylistviewapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
